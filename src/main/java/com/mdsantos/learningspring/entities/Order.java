ff1181ec3c549e9f8395240a732f5c8428bc5ce6
Michel Santos <michelsantosk2@gmail.com>
2023-01-04 19:05:46 -0300

    feat: OrderStatus enum

--- src/main/java/com/mdsantos/learningspring/entities/Order.java
+++ src/main/java/com/mdsantos/learningspring/entities/Order.java
@@ -1,6 +1,7 @@
 package com.mdsantos.learningspring.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.mdsantos.learningspring.entities.enums.OrderStatus;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -19,15 +20,18 @@ public class Order implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
+    private Integer orderStatus;
+
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
 
     public Order(){}
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -53,6 +57,16 @@ public class Order implements Serializable {
 
     public void setClient(User client) {
         this.client = client;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     @Override
